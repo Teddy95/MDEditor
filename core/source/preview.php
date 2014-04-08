7fd97133d81b4f94cf50bd27eff008fe3f15d293
Andre Sieverding <webmaster@andre-sieverding.de>
2014-04-08 17:33:36 +0200

    Delete preview.php

--- core/source/preview.php
+++
@@ -1,7 +0,0 @@
-<?php
-$path = dirname(__FILE__) . '/../';
-require_once($path . 'markdown/Michelf/MarkdownExtra.inc.php');
-
-use \Michelf\MarkdownExtra;
-echo "<div class='MDEditor_body_actioncontainer_content_preview'>" . MarkdownExtra::defaultTransform($_GET['markup']) . "</div>";
-?>
